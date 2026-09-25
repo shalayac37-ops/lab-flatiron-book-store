be93733a1777eebfc65f051a5719487b7969c5c8
shalaya <shalayac37@gmail.com>
2026-09-24 22:52:25 -0400

    Complete Flatiron Book Store lab

--- index.js
+++ index.js
@@ -44,4 +44,22 @@ const bookStore = {
 }
 
 // Write your code here!
+const bookStoreTitle = document.getElementById("header");
+bookStoreTitle.textContent = bookStore.name;
+
+const bookList = document.getElementById("book-list");
+
+bookStore.books.forEach((book) => {
+  const bookContainer = document.createElement("li");
+  const bookTitle = document.createElement("h3");
+  const bookAuthor = document.createElement("p");
+  const bookImage = document.createElement("img");
+
+  bookTitle.textContent = book.title;
+  bookAuthor.textContent = book.author;
+  bookImage.src = book.imageUrl;
+
+  bookContainer.append(bookTitle, bookAuthor, bookImage);
+  bookList.append(bookContainer);
+});
 
